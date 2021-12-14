d688774751a9d94fcd69a24baf1d737e6112468c
akshayyadav11 <akshayy976@gmail.com>
2021-12-14 16:25:21 +0530

    first commit

---
+++ calc/views.py
@@ -0,0 +1,15 @@
+from os import name
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    # return HttpResponse('Hello World')
+    return render(request, 'home.html',{'name':'akshay'})
+
+def add(request):
+    val1 = request.POST['num1']
+    val2 = request.POST['num2']
+    res = int(val1) + int(val2)
+    return render(request, 'result.html',{'addition':res})
